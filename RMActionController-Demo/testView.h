d3ca5eda1a57797eeb7b298d083fddb127e8e23d
Admin <admin@Rash-Macbook.local>
2016-10-25 16:16:47 +0330

    try to change for suing in anothrer priject

---
+++ RMActionController-Demo/testView.h
@@ -0,0 +1,13 @@
+//
+//  testView.h
+//  RMActionController-Demo
+//
+//  Created by Admin on 10/25/16.
+//  Copyright © 2016 Roland Moers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testView : UIView
+
+@end
---
+++ RMActionController-Demo/testView.m
@@ -0,0 +1,21 @@
+//
+//  testView.m
+//  RMActionController-Demo
+//
+//  Created by Admin on 10/25/16.
+//  Copyright © 2016 Roland Moers. All rights reserved.
+//
+
+#import "testView.h"
+
+@implementation testView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
